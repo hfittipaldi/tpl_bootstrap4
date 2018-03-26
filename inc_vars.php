84578fa69e85c3c14ce7c6103200c6fd120c65d6
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2018-03-26 14:45:47 -0300

    Add variables

--- inc_vars.php
+++ inc_vars.php
@@ -9,7 +9,52 @@
 
 defined('_JEXEC') or die;
 
+// Returns a reference to the application object
+$app = JFactory::getApplication();
+
+// Returns a reference to the global document object
 $doc = JFactory::getDocument();
+
+// Returns a reference to the user object
+$user = JFactory::getUser();
+
+// Returns a reference to the menu object
+$menu = $app->getMenu();
+
+// Get the active menu
+$menu_active = $menu->getActive();
+
+// Overwrite the language
+$this->language = $doc->language;
+
+// Overwrite the text direction
+$this->direction = $doc->direction;
+
+// Get the tamplate path
+$this->template_path = $this->baseurl . '/templates/' . $this->template;
+
+// Output as HTML5
+$doc->setHtml5(true);
+
+// Getting params from template
+$tpl_params = $app->getTemplate(true)->params;
+
+// Detecting Active Variables
+$jinput   = $app->input;
+$option   = $jinput->get('option', '');
+$view     = $jinput->get('view', '');
+$layout   = $jinput->get('layout', '');
+$task     = $jinput->get('task', '');
+$itemid   = $jinput->getInt('Itemid', '');
+$sitename = htmlspecialchars($app->get('sitename', ''), ENT_QUOTES, 'UTF-8');
+
+// Get pageclass suffix
+$pageclass = '';
+if (is_object($menu_active))
+{
+    $pageclass = $menu_active->params->get('pageclass_sfx');
+}
+
 
 // Add Stylesheets
 JHtmlBootstrap::loadCss(true, $this->direction);
